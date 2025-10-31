ff229fdd55fc1d2d31f8d3e358ede7478a84d4ce
Hisana125 <hisanahamza125@gmail.comgit config --global user.name Hisana125git config --global user.email hisanahamza125@gmail.com>
2025-10-31 10:06:11 +0530

    fibonacci using recursion

---
+++ fibonacci_recursion.py
@@ -0,0 +1,10 @@
+def fibonacci(num):
+    """Return the nth Fibonacci number using Recursion."""
+    if num<=1:
+        return num
+    else:
+        return fibonacci(num-1)+fibonacci(num-2)
+n=int(input("Enter the number of terms:"))
+print("fibonacci series:")
+for i in range(n):
+    print(fibonacci(i),end=" ")
