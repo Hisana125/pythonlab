150a299a49dc741c8e36c446b88cdc1bee67d95a
Hisana125 <hisanahamza125@gmail.comgit config --global user.name Hisana125git config --global user.email hisanahamza125@gmail.com>
2025-11-07 15:01:15 +0530

    area package

---
+++ import.py
@@ -0,0 +1,6 @@
+from graphics.rectangle import *
+from graphics.circle import *
+from graphics.ThreeD_graphics.sphere import *
+print("Rectangle Perimeter:",perimeter(4,6))
+print("Circle Area:",area(5))
+print("Sphere Perimeter:",perimeter(3))
